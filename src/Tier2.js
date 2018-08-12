01542b8af82f5eb1a75d8e2a86c98a32bcae2c56
Monika Williams <monika.m.williams@gmail.com>
2018-08-12 01:46:05 +0000

    automatically backed up by learn

---
+++ src/Tier2.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { getRandomColor, getReducedColor } from './randomColorGenerator.js'
+import Tier3 from './Tier3'
+
+
+export default class Tier2 extends Component {
+
+  render() {
+    // hard coded color values have been added below, though they won't be
+    // present in our solution. What should they be replaced with?
+    return (
+      <div className="tier2" onClick={this.props.handleClick} style={{backgroundColor: this.props.color, color: this.props.color}}>
+        <Tier3 handleClick={this.props.handleChildClick} color={this.props.childColor} />
+        <Tier3 handleClick={this.props.handleChildClick} color={this.props.childColor} />
+      </div>
+    )
+  }
+}
